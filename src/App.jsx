8269b53bdfcbb3dc6012187cf2785d92839600dd
Facundo <facundojsole@gmail.com>
2023-03-02 22:08:27 -0300

    add favicon and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
 
   return (
-    <div className="font-main font-light p-6">
+    <div className="font-main font-normal p-6">
       <h1 className="text-2xl font-light">Know everything about your <span className="rounded-full bg-red-600 border border-solid border-black px-6 py-2 text-white">Pokemon!</span></h1>
+      
     </div>
   )
 }

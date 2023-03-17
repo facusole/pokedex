cc626d5e6bcd6bfac11dfaa8c04cecf2214ff3f8
Facundo <facundojsole@gmail.com>
2023-03-17 19:23:36 -0300

    change padding so the border uses full vw

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import SearchBar from "./Components/SearchBar"
 function App() {
 
   return (
-    <div className="font-main font-normal text-xl p-6 bg-background ">
-      <nav className="md:flex md:items-center md:justify-between lg:gap-5 lg:justify-start sticky top-0 z-50 bg-background py-6">
+    <div className="font-main font-normal text-xl bg-background">
+      <nav className="md:flex md:items-center md:justify-between lg:gap-5 lg:justify-start sticky top-0 z-50 bg-background p-6 border-b-[1px]">
         <h1 className="text-base font-light md:text-2xl md:font-medium">Know everything about your <span className="rounded-full bg-main-red border border-solid border-black px-6 py-2 text-white text-base md:font-normal md:px-4">Pokemon!</span></h1>
         <SearchBar/>
       </nav>

9a224c53d07e1d5f12b3525de07ecd1f78ec5e50
Facundo <facundojsole@gmail.com>
2023-03-22 19:58:30 -0300

    change margin in detailpage

--- src/Components/PokemonDetailPage.jsx
+++ src/Components/PokemonDetailPage.jsx
@@ -12,7 +12,7 @@ export default function PokemonDetailPage({ pokemon, openAnimationRef, handleDet
                 <span className="flex items-center transition"><ArrowCircleLeft2 size="24" />Go back</span></button>
             {pokemon ?
                 <div className="text-center font-medium">
-                    <div className="h-[95.5vh] overflow-y-auto scrollbar-hide flex flex-col justify-start items-center m-2 border border-solid border-black rounded-3xl lg:h-[87vh]">
+                    <div className="h-[95.5vh] py-4 overflow-y-auto scrollbar-hide flex flex-col justify-start items-center m-2 border border-solid border-black rounded-3xl lg:h-[87vh]">
                         <div className="border border-solid border-black rounded-3xl bg-white my-4 relative">
                             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
                             <span className="absolute top-[-12px] right-0 bg-white border border-solid border-black rounded-full py-1 px-4 text-sm">{pokemon.id < 100 ? ('#0' + pokemon.id).slice(-5) : ('#' + pokemon.id).slice(-5)}</span>
@@ -20,18 +20,18 @@ export default function PokemonDetailPage({ pokemon, openAnimationRef, handleDet
                         <div className="gap-1 items-center h-2">
                             <h1 className="capitalize text-xl my-2 md:text-2xl">{pokemon.name}</h1>
                             <PokemonDetailPageTypesLabel pokemonTypes={pokemon.types} />
-                            <div className="px-6 py-6 my-6 bg-white border border-solid border-black rounded-3xl grid place-items-center gap-4">
+                            <div className="p-6 my-6 bg-white border border-solid border-black rounded-3xl grid place-items-center gap-4">
                                 <div className="flex items-center gap-2"><span className="rounded-full border border-black px-3 py-1 w-[8ch] bg-purple-200 mx-auto">{pokemon.height * 10 + 'cm'}</span><span className="rounded-full border border-black px-3 py-1 w-[8ch] bg-purple-200 mx-auto">{pokemon.weight / 10 + "kg"}</span></div>
                                 <div className="flex items-center gap-2"><span className="rounded-full border border-black px-3 py-1 w-[8ch] mx-auto">Height</span><span className="rounded-full border border-black px-3 py-1 w-[8ch] mx-auto">Weight</span></div>
                             </div>
-                            <div className="p-6 bg-white border border-solid border-black rounded-3xl grid place-items-center gap-4">
+                            <div className="p-6 bg-white border border-solid border-black rounded-3xl grid place-items-center gap-4 my-6">
                                 <h3>Abilities</h3>
                                 <div className="flex gap-3">
                                     <p className="px-4 py-1 bg-[#EEFFE3] border border-solid border-black rounded-full capitalize">{pokemon.abilities[0].ability.name}</p>
                                     {pokemon.abilities[1] ? <p className="px-4 py-1 bg-[#EEFFE3] border border-solid border-black rounded-full capitalize">{pokemon.abilities[1].ability.name}</p> : null}
                                 </div>
                             </div>
-                            <div className="px-6 py-4 my-2 bg-white border border-solid border-black rounded-3xl">
+                            <div className="px-6 py-4 my-6 bg-white border border-solid border-black rounded-3xl">
                                 <h3>Stats</h3>
                                 <ul>
                                     <li className="flex justify-between items-center gap-8 my-2"><span className="rounded-full border border-black px-4 py-2 w-[12ch] md:text-2xl">HP</span> <span className="rounded-full border border-black px-4 py-2 w-[6ch] bg-purple-200 mx-auto">{pokemon.stats[0].base_stat}</span></li>
